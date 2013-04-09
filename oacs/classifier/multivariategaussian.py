88d7b61d0afe454ad78aeb5e3837076548e84d1a
Stephen L <lrq3000@gmail.com>
2013-04-10 01:12:17 +0200

    add: Univariate Gaussian and Multivariate Gaussian classifiers
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

---
+++ oacs/classifier/multivariategaussian.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+## @package multivariategaussian
+#
+# Multivariate gaussian AIS (Artificial Immune System) classifier.
+
+from oacs.base import BaseClass
+import random
+import numpy as np
+import pandas as pd
+from numpy import pi, exp
+
+## MultivariateGaussian
+#
+# Multivariate gaussian AIS (Artificial Immune System) classifier class, this will return a set of parameters: a vector of means Mu, and a covariance matrix Sigma2
+# This AIS classifier can catch any correlation between any feature
+class MultivariateGaussian(BaseClass):
+
+    ## @var config
+    # An instance of the ConfigParser object, already loaded
+
+    ## Constructor
+    # @param config An instance of the ConfigParser class
+    def __init__(self, config, *args, **kwargs):
+        return BaseClass.__init__(self, config, *args, **kwargs)
+
+    ## Learn the parameters from a given set X of examples, and labels Y
+    # @param X Samples set
+    # @param Y Labels set (corresponding to X)
+    def learn(self, X=None, Y=None, *args, **kwargs):
+        Yt = Y[Y==0].dropna() # get the list of non-anomalous examples
+        Xt = X.irow(Yt.index) # filter out anomalous examples and keep only non-anomalous ones
+        Mu = self.mean(Xt, Xt['framerepeat']) # Mean
+        Sigma2 = self.covar(Xt, Mu, 'framerepeat') # Vector of variances or Covariance matrix
+        return {'Mu': Mu, 'Sigma2': Sigma2} # always return a dict of variables
+
+    ## Multivariate gaussian prediction of the probability/class of an example given a set of parameters (weighted mean and covariance matrix)
+    # @param X One unknown example to label
+    # @param Mu Weighted mean of X
+    # @param Sigma2 Covariance matrix of X
+    # TODO: compatibility with more than one sample (detect type==pdSseries)?
+    def predict(self, x=None, Mu=None, Sigma2=None, *args, **kwargs):
+        if 'framerepeat' in x:
+            x = x.drop(['framerepeat'])
+            Sigma2 = Sigma2.drop(['framerepeat'], axis=0) # drop in both axis
+            Sigma2 = Sigma2.drop(['framerepeat'], axis=1)
+            Mu = Mu.drop(['framerepeat'])
+
+        # if sigma2 is a vector, we convert it to a (covariance) matrix (filled with only values on the diagonal)
+        if type(Sigma2) == pd.Series:
+            Sigma2 = np.diag(Sigma2)
+            Sigma2 = pd.DataFrame(Sigma2)
+
+        n = len(Mu.keys()) #X.shape[0]
+        xm = x-Mu # X difference to the mean
+        xm = xm.fillna(0) # if we have one NA, the whole result of all values will be NA
+        Pred = (2*pi)**(-n/2) * np.linalg.det(Sigma2)**0.5 * exp(-0.5 * xm.T.dot(np.linalg.pinv(Sigma2)).dot(xm))
+
+        return {'Pred': Pred} # return the class of the sample(s)
+
+    ## Compute the weighted mean of the dataset
+    # @param X Samples dataset
+    # @param weights Vector/Series of weights (ie: number of times one sample has to be repeated) - default: X['framerepeat']
+    # TODO: bigdata iteration version (detect generator?)
+    def mean(self, X, weights=None):
+        if weights is None: weights = X['framerepeat']
+        mean = np.average(X, axis=0, weights=weights)
+        mean = pd.Series(mean, index=list(X.keys()))
+        return mean
+
+    ## Compute the weighted covariance matrix of the dataset
+    # Alternative to pandas.DataFrame.cov(), because pandas's and numpy's cov() can't account for weights (if you set mean = X.mean(), then you'll get the exact same result as X.cov())
+    # @param X One example or a dataset of examples (must the same columns/keys as mean)
+    # @param mean Weighted mean (must have the same columns/keys as X, else you will get a weird result, because pandas will still try to adapt and things will get really messed up!)
+    # @param weights Name of the weights column to remove from the final result (else it may flaw the computation of the prediction)
+    # TODO: bigdata iteration version (detect generator?) - WARNING: then the division by m-1 must be done at the end of all the sums of all sigma2 of every x sample!
+    def covar(self, X, mean, weights=None):
+        if weights is None: weights = 'framerepeat'
+        if weights in X.keys() and weights not in mean.keys():
+            if type(X) == pd.Series:
+                ax = 0
+            else:
+                ax = 1
+            X = X.drop(weights, axis=ax)
+        xm = X-mean # xm = X diff to mean
+        xm = xm.fillna(0)
+        if type(X) == pd.Series:
+            sigma2 = np.outer(xm.T, xm); # force matrix multiplication outer dot product (else if you use np.dot() or pandas.dot(), it will align by the indexes)
+        else:
+            m = X.shape[0]
+            sigma2 = 1./(m-1) * xm.T.dot(xm);
+
+        return sigma2
--- oacs/classifier/univariategaussian.py
+++ oacs/classifier/univariategaussian.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-## @package base
+## @package univariategaussian
 #
-# This contains the base classifier class to be used as a template for other classifiers
+# Univariate gaussian AIS (Artificial Immune System) classifier.
 
 from oacs.base import BaseClass
 import random
+import numpy as np
+import pandas as pd
+from numpy import pi, exp
 
-## BaseClassifier
+## UnivariateGaussian
 #
-# Base classifier class, use this as a template for your classifier algorithm
-class BaseClassifier(BaseClass):
+# Univariate gaussian AIS (Artificial Immune System) classifier class, this will return a set of parameters: a vector of means Mu, and a vector of variances Sigma2 for each feature
+class UnivariateGaussian(BaseClass):
 
     ## @var config
     # An instance of the ConfigParser object, already loaded
@@ -25,11 +28,75 @@ class BaseClassifier(BaseClass):
     # @param X Samples set
     # @param Y Labels set (corresponding to X)
     def learn(self, X=None, Y=None, *args, **kwargs):
-        Theta = [1]
-        return {'Theta': [1]} # always return a dict of variables
+        Yt = Y[Y==0].dropna() # get the list of non-anomalous examples
+        Xt = X.irow(Yt.index) # filter out anomalous examples and keep only non-anomalous ones
+        Mu = self.mean(Xt, Xt['framerepeat']) # Mean
+        Sigma2 = self.variance(Xt, Mu, Xt['framerepeat']) # Vector of variances or Covariance matrix
+        return {'Mu': Mu, 'Sigma2': Sigma2} # always return a dict of variables
 
-    ## Predict/Classify the probability/class of an example given a set of parameters
+    ## Univariate gaussian prediction of the probability/class of an example given a set of parameters (weighted mean and vector of standard deviations)
     # @param X One unknown example to label
-    # @param Theta Set of parameters to use for the prediction/classification (should be outputted by the local method learn() )
-    def predict(self, X=None, Theta=None, *args, **kwargs):
-        return {"Prediction": random.uniform(0.0,1.0) } # return the class of the sample (here we make no prediction, we just return a random probability between 0 and 1)
+    # @param Mu Weighted mean of X
+    # @param Sigma2 Covariance matrix of X
+    def predict(self, x=None, Mu=None, Sigma2=None, *args, **kwargs):
+        # Univariate gaussian density estimation
+        Pred = (1/(2*pi*Sigma2)**0.5) * exp(-(x-Mu)/(2*Sigma2))
+        if 'framerepeat' in Pred.keys():
+            Pred = Pred.drop(['framerepeat'])
+        # Compute the product of all probabilities (p1 = proba of feature 1 being normal; p1*p2*p3*...*pn)
+        Pred = Pred.prod()
+
+        return {'Pred': Pred} # return the class of the sample(s)
+
+    ## Compute the weighted mean of the dataset
+    # @param X Samples dataset
+    # @param weights Vector/Series of weights (ie: number of times one sample has to be repeated) - default: X['framerepeat']
+    # TODO: bigdata iteration version (detect generator?)
+    def mean(self, X, weights=None):
+        if weights is None: weights = X['framerepeat']
+        mean = np.average(X, axis=0, weights=weights)
+        mean = pd.Series(mean, index=list(X.keys()))
+        return mean
+
+    ## Alternative way to compute the weighted mean of the dataset without using Numpy (ends up being slower)
+    # @param X Samples dataset
+    # @param weights Vector/Series of weights (ie: number of times one sample has to be repeated) - default: X['framerepeat']
+    def mean_alt(self, X, weights=None):
+        def applyweight(serie):
+            if weights is not None:
+                weight = serie[weights]
+            else:
+                weight = serie['framerepeat']
+            #s2 = serie.drop(['framerepeat'])
+            s = serie * weight
+            s['framerepeat'] = weight
+            return s
+
+        if 'framerepeat' in X.keys():
+            X = X.apply(applyweight, axis=1)
+
+        mean = X.sum().astype(float) / X.ix[:,'framerepeat'].sum()
+        return mean
+
+    ## Compute the weighted unbiased variance of each feature for a given dataset
+    # @param X Samples dataset
+    # @param mean Weighted mean
+    # @param weights Vector/Series of weights (ie: number of times one sample has to be repeated) - default: X['framerepeat']
+    # TODO: bigdata iteration version (detect generator?)
+    def variance(self, X, mean, weights=None):
+        if weights is None: weights = X['framerepeat']
+        squareddiff = (X-mean)**2
+        variance = squareddiff.T.dot(weights) / (weights.sum()-1) # DataFrames and Series are implicitly aligned by index
+        #sigma = variance ** 0.5
+        return variance
+
+    ## Alternative way to compute the weighted unbiased variance of each feature for a given dataset using numpy instead of pandas (this is actually slower)
+    # @param X Samples dataset
+    # @param mean Weighted mean
+    # @param weights Vector/Series of weights (ie: number of times one sample has to be repeated) - default: X['framerepeat']
+    def variance_alt(self, X, mean, weights=None):
+        if weights is None: weights = X['framerepeat']
+        variance = np.dot(weights.tolist(), ((X-mean.tolist())**2))/ (weights.sum()-1)  # Fast and numerically precise
+        #sigma = np.sqrt(variance)
+        variance = pd.Series(variance, index=list(X.keys()))
+        return variance
