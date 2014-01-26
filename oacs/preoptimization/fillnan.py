6a82424eb92d789557d3cd1574837cc6fb2fedc2
Stephen L <lrq3000@gmail.com>
2014-01-26 07:42:28 +0100

    add: FillNaN supports mean mode
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- oacs/preoptimization/fillnan.py
+++ oacs/preoptimization/fillnan.py
@@ -6,6 +6,7 @@
 # Fill NaNs by copying other observed values (forward fill then backward fill)
 
 from oacs.preoptimization.basepreoptimization import BasePreOptimization
+from oacs.classifier.univariategaussian import UnivariateGaussian
 
 ## FillNaN
 #
@@ -24,12 +25,18 @@ class FillNaN(BasePreOptimization):
     ## Fill NaNs by copying other observed values (forward fill then backward fill)
     # @param X Samples set
     def optimize(self, X=None, *args, **kwargs):
-        # Forward fill first, which means we copy subsequent values of a feature to previous NaNs occurrences (because most of the NaNs will be because of the first frames when the engine could not set correctly all variables, so it's better to first begin to forward fill, this should be more reliable in our case)
-        X = X.fillna(method='ffill')
-        # Backward fill remaining NaNs
-        X = X.fillna(method='bfill')
-        # Check that we have filled all NaNs
-        #if (sum(X.count() < X.shape[0])):
-        #    print("FillNan warning: could not fill all NaNs, some NaNs are still in the dataset! Mean, variance and other classifier computations will probably fail!")
+        mode = self.config.get("fillnan_mode", "mean");
+        if mode == "mean":
+            Mean = UnivariateGaussian.mean(X);
+            X = X.fillna(value=Mean, axis=0);
+        else:
+            # Forward fill first, which means we copy subsequent values of a feature to previous NaNs occurrences (because most of the NaNs will be because of the first frames when the engine could not set correctly all variables, so it's better to first begin to forward fill, this should be more reliable in our case)
+            X = X.fillna(method='ffill', axis=0)
+            # Backward fill remaining NaNs
+            X = X.fillna(method='bfill', axis=0)
+            # Check that we have filled all NaNs
+            #if (sum(X.count() < X.shape[0])):
+            #    print("FillNan warning: could not fill all NaNs, some NaNs are still in the dataset! Mean, variance and other classifier computations will probably fail!")
+
         # Return the NaNs-filled samples set
         return {'X':  X } # always return a dict of variables if you want your variables saved durably and accessible later
